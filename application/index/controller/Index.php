b36d1e0665895babe2e7925bd9b6318623f56c29
DM <769245396@qq.com>
2017-04-16 15:58:58 +0800

    初始化

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,6 +5,23 @@ class Index
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
+        return '
+        <style>
+            .out{height: 100%;justify-content: center;align-items: center;display: flex;}
+            .in{line-height: 2.8em;font-family: 黄彦文行书字体,良怀行书,行书,行楷,楷体;font-size: 30px;writing-mode: tb-rl;}
+        </style>
+        <div class="out">
+            <div class="in">
+            弃我去者<br>昨日之日不可留<br>
+            乱我心者<br>今日之日多烦忧<br>
+            长风万里送秋雁<br>对此可以酣高楼<br>
+            蓬莱文章建安骨<br>中间小谢又清发<br>
+            俱怀逸兴壮思飞<br>欲上青天揽明月<br>
+            抽刀断水水更流<br>举杯消愁愁更愁<br>
+            人生在世不称意<br>明朝散发弄扁舟<br>
+            ——李白
+            </div>
+        </div>
+        ';
     }
 }
