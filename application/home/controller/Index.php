dde9e4f8ec02f73c89c2adfed84d22a5e16135e5
DM <769245396@qq.com>
2017-04-16 17:55:50 +0800

    新建home模块

---
+++ application/home/controller/Index.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return '
+        <style>
+            .out{height: 100%;justify-content: center;align-items: center;display: flex;}
+            .in{line-height: 2.8em;font-family: 黄彦文行书字体,良怀行书,行书,行楷,楷体;font-size: 30px;writing-mode: tb-rl;}
+        </style>
+        <div class="out">
+            <div class="in">
+            弃我去者<br>昨日之日不可留<br>
+            乱我心者<br>今日之日多烦忧<br>
+            长风万里送秋雁<br>对此可以酣高楼<br>
+            蓬莱文章建安骨<br>中间小谢又清发<br>
+            俱怀逸兴壮思飞<br>欲上青天揽明月<br>
+            抽刀断水水更流<br>举杯消愁愁更愁<br>
+            人生在世不称意<br>明朝散发弄扁舟<br>
+            ——李白
+            </div>
+        </div>
+        ';
+    }
+    public function test(){
+        return view('index',['name'=>'maoge']);
+    }
+}
