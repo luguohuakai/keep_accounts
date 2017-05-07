bcac88ee228c04064718bc669b0b257a433c31db
DM <769245396@qq.com>
2017-05-07 23:17:55 +0800

    phpoffice/phpexcel mpdf excel等的导出

--- application/common.php
+++ application/common.php
@@ -11,7 +11,7 @@
 
 // 应用公共文件
 
-if(!function_exists('send_email')){
+if (!function_exists('send_email')) {
     /**
      * 功能函数 - 发送email
      * @param string | array $toemail 要发送到的email地址, 多个使用一维数组即可
@@ -20,7 +20,8 @@ if(!function_exists('send_email')){
      * @return bool
      * @throws phpmailerException
      */
-    function send_email($toemail, $subject, $body) {
+    function send_email($toemail, $subject, $body)
+    {
         //vendor模式
 //        vendor('phpmailer.phpmailer#class');
 //        $mail = new PHPMailer();
@@ -36,13 +37,13 @@ if(!function_exists('send_email')){
         //加密方式 "ssl" or "tls"
         $mail->SMTPSecure = config('email_config.secure'); //这里要注意, QQ发送邮件使用的ssl方式,如果不设置, 则会失败! 请认真查看下面的配置文件!!!
         //smtp需要鉴权 这个必须是true
-        $mail->SMTPAuth=true;
+        $mail->SMTPAuth = true;
         //链接qq域名邮箱的服务器地址
         $mail->Host = config('email_config.host');
         //设置ssl连接smtp服务器的远程服务器端口号 可选465或587
         $mail->Port = config('email_config.port');
         //smtp登录的账号 这里填入字符串格式的qq号即可
-        $mail->Username =config('email_config.username');
+        $mail->Username = config('email_config.username');
         //smtp登录的密码 这里填入“独立密码” 若为设置“独立密码”则填入登录qq的密码 建议设置“独立密码”
         $mail->Password = config('email_config.psw');
         //设置发件人邮箱地址 这里填入上述提到的“发件人邮箱”
@@ -56,11 +57,11 @@ if(!function_exists('send_email')){
         $mail->isHTML(true);
         //设置收件人邮箱地址 该方法有两个参数 第一个参数为收件人邮箱地址 第二参数为给该地址设置的昵称 不同的邮箱系统会自动进行处理变动 这里第二个参数的意义不大
         // 添加收件人地址，可以多次使用来添加多个收件人
-        if(is_array($toemail)){
-            foreach($toemail as $to_email){
+        if (is_array($toemail)) {
+            foreach ($toemail as $to_email) {
                 $mail->AddAddress($to_email);
             }
-        }else{
+        } else {
             $mail->AddAddress($toemail);
         }
         //添加该邮件的主题
@@ -79,12 +80,127 @@ if(!function_exists('send_email')){
         $rs = $mail->send();
 
         //简单的判断与提示信息
-        if($rs === true) {
+        if ($rs === true) {
             //echo 'success';
             return true;
-        }else{
+        } else {
             //dump($mail->ErrorInfo);
             return false;
+        }
+    }
+}
+
+if (!function_exists('out_to_excel')) {
+    function out_to_excel($data, $name = 'Excel')
+    {
+        error_reporting(E_ALL);
+        date_default_timezone_set('Europe/London');
+        $objPHPExcel = new PHPExcel();
+
+        /*以下是一些设置 ，什么作者  标题啊之类的*/
+        $objPHPExcel->getProperties()->setCreator("脆香甜")
+            ->setLastModifiedBy("脆香甜")
+            ->setTitle("数据EXCEL导出")
+            ->setSubject("数据EXCEL导出")
+            ->setDescription("备份数据")
+            ->setKeywords("excel")
+            ->setCategory("result file");
+        /*以下就是对处理Excel里的数据， 横着取数据，主要是这一步，其他基本都不要改*/
+        $i = 1;
+        foreach ($data as $k => $v) {
+            $j = ord('A');
+            $k = 0;
+            foreach ($v as $kk => $vv) {
+                if ($i != 1) {
+                    $objPHPExcel->setActiveSheetIndex(0)
+                        //Excel的第A列，uid是你查出数组的键值，下面以此类推
+                        ->setCellValue(chr($j) . $i, $vv);
+                } else {
+                    $objPHPExcel->setActiveSheetIndex(0)
+                        //Excel的第A列，uid是你查出数组的键值，下面以此类推
+                        ->setCellValue(chr($j) . 1, $kk)
+                        ->setCellValue(chr($j) . 2, $vv);
+                    if ($k == count($v) - 1) {
+                        $i++;
+                    }
+                    $k++;
+                }
+                $j++;
+            }
+            $i++;
+        }
+
+        $objPHPExcel->getActiveSheet()->setTitle('User');
+        $objPHPExcel->setActiveSheetIndex(0);
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="' . $name . '.xls"');
+        header('Cache-Control: max-age=0');
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+        $objWriter->save('php://output');
+        exit;
+    }
+}
+
+if (!function_exists('excel_to_database')) {
+    function excel_to_database()
+    {
+        if (!empty ($_FILES ['file_excel'] ['name'])) {
+            $tmp_file = $_FILES ['file_excel'] ['tmp_name'];
+            $file_types = explode(".", $_FILES ['file_excel'] ['name']);
+            $file_type = $file_types [count($file_types) - 1];
+
+            /*判别是不是.xls文件，判别是不是excel文件*/
+            if (strtolower($file_type) != "xls") {
+                $this->error('不是Excel文件，重新上传');
+            }
+
+            /*设置上传路径*/
+            $savePath = SITE_PATH . '/public/upfile/Excel/';
+
+            /*以时间来命名上传的文件*/
+            $str = date('Ymdhis');
+            $file_name = $str . "." . $file_type;
+
+            /*是否上传成功*/
+            if (!copy($tmp_file, $savePath . $file_name)) {
+                $this->error('上传失败');
+            }
+
+            /*
+        
+               *对上传的Excel数据进行处理生成编程数据,这个函数会在下面第三步的ExcelToArray类中
+        
+              注意：这里调用执行了第三步类里面的read函数，把Excel转化为数组并返回给$res,再进行数据库写入
+        
+            */
+            $res = Service('ExcelToArray')->read($savePath . $file_name);
+
+            /*
+         
+                 重要代码 解决Thinkphp M、D方法不能调用的问题  
+         
+                 如果在thinkphp中遇到M 、D方法失效时就加入下面一句代码
+         
+             */
+            //spl_autoload_register ( array ('Think', 'autoload' ) );
+
+            /*对生成的数组进行数据库的写入*/
+            foreach ($res as $k => $v) {
+                if ($k != 0) {
+                    $data ['uid'] = $v [0];
+                    $data ['password'] = sha1('111111');
+                    $data ['email'] = $v [1];
+
+                    $data ['uname'] = $v [3];
+
+                    $data ['institute'] = $v [4];
+                    $result = M('user')->add($data);
+                    if (!$result) {
+                        $this->error('导入数据库失败');
+                    }
+                }
+            }
+
         }
     }
 }
--- application/home/controller/Demo.php
+++ application/home/controller/Demo.php
@@ -8,27 +8,154 @@
 
 namespace app\home\controller;
 
+use PHPExcel;
+use PHPExcel_IOFactory;
 
 class Demo
 {
     // 展示表格
-    public function showTable(){
+    public function showTable()
+    {
         return view();
     }
 
     // 展示表单
-    public function showForm(){
+    public function showForm()
+    {
         return view();
     }
 
     // 展示更多
-    public function showMore(){
+    public function showMore()
+    {
         return view();
     }
 
     // 邮件发送
-    public function send_email(){
-        $rs = send_email('1102313831@qq.com','maoge','<h1>Hello</h1><h2 style="color: red;">你好</h2>');
+    public function sendEmail()
+    {
+        $rs = send_email('1102313831@qq.com', 'maoge', '<h1>Hello</h1><h2 style="color: red;">你好</h2>');
         dump($rs);
     }
+
+    // 导出到excel
+    public function outToExcel()
+    {
+        $rs = [['name' => 'zs', 'age' => 12, 'sex' => '男'], ['name' => 'xh', 'age' => 15, 'sex' => '女']];
+        $rss = out_to_excel($rs);
+        dump($rss);
+    }
+
+    // 导出到excel
+    public function outToExcel2()
+    {
+        $objPHPExcel = new PHPExcel();
+        // 第一步 设置文件属性
+        $objPHPExcel
+            ->getProperties()//获得文件属性对象，给下文提供设置资源
+            ->setCreator('脆香甜')//设置文件的创建者
+            ->setLastModifiedBy('脆香甜')//设置最后修改者
+            ->setTitle('excel_demo1')//设置标题
+            ->setSubject('excel_demo2')//设置主题
+            ->setDescription('excel_demo3')//设置描述
+            ->setKeywords('excel_demo4')//设置标记
+            ->setCategory('excel_demo5');//设置类别
+        // 第二步 给表格添加数据
+        $objPHPExcel
+            ->setActiveSheetIndex(0)//设置第一个内置表（一个xls文件里可以有多个表）为活动的
+            ->setCellValue('A1', 'Hello')//给表的单元格设置数据
+            ->setCellValue('B2', 'world!')//数据格式可以为字符串
+            ->setCellValue('C1', 12)//数字型
+            ->setCellValue('D2', 122)
+            ->setCellValue('D3', true)//布尔型
+            ->setCellValue('D4', '=SUM(C1:D2)');//公式
+        // 第三步 得到当前活动的表,注意下文教程中会经常用到$objActSheet
+        $objActSheet = $objPHPExcel->getActiveSheet();
+        $objActSheet->setTitle('excel_demo6');//给当前活动的表设置名称
+
+        // 直接生成一个文件
+//        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+//        $rs1 = $objWriter->save('myexchel.xlsx');
+//        dump($rs1);//null
+
+        // 提示下载文件 excel2003
+//        header('Content-Type: application/vnd.ms-excel');
+//        header('Content-Disposition: attachment;filename="01simple.xls"');
+//        header('Cache-Control: max-age=0');
+//        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+//        $objWriter->save('php://output');
+//        exit;
+
+        // 提示下载文件 excel2007
+//        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+//        header('Content-Disposition: attachment;filename="01simple.xlsx"');
+//        header('Cache-Control: max-age=0');
+//        $objWriter = PHPExcel_IOFactory:: createWriter($objPHPExcel, 'Excel2007');
+//        $objWriter->save('php://output');
+//        exit;
+
+        // 提示下载文件 pdf 不支持?
+//        $rendererName = \PHPExcel_Settings::PDF_RENDERER_MPDF;
+//        $rendererLibraryPath = '../vendor/mpdf/mpdf';
+//        if (!\PHPExcel_Settings::setPdfRenderer(
+//            $rendererName,
+//            $rendererLibraryPath
+//        )) {
+//            die(
+//                'NOTICE: Please set the $rendererName and $rendererLibraryPath values' .
+//                '<br />' .
+//                'at the top of this script as appropriate for your directory structure'
+//            );
+//        }
+//        header('Content-Type: application/pdf');
+//        header('Content-Disposition: attachment;filename="01simple.pdf"');
+//        header('Cache-Control: max-age=0');
+//        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'PDF');
+//        $objWriter->save('php://output');
+//        exit;
+
+        // 生成一个pdf文件
+//        $rendererName = \PHPExcel_Settings::PDF_RENDERER_MPDF;
+//        $rendererLibraryPath = '../vendor/mpdf/mpdf';
+//        if (!\PHPExcel_Settings::setPdfRenderer(
+//            $rendererName,
+//            $rendererLibraryPath
+//        )) {
+//            die(
+//                'NOTICE: Please set the $rendererName and $rendererLibraryPath values' .
+//                '<br />' .
+//                'at the top of this script as appropriate for your directory structure'
+//            );
+//        }
+//        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'PDF');
+//        $rs2 = $objWriter->save('a.pdf');
+//        dump($rs2);die;
+
+        // 生成CSV文件
+//        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'CSV')
+//            ->setDelimiter(',')//设置分隔符
+//            ->setEnclosure('"')//设置包围符
+//            ->setLineEnding("\r\n")//设置行分隔符
+//            ->setSheetIndex(0)//设置活动表
+//            ->save(str_replace('.php', '.csv', __FILE__));
+//        dump($objWriter);die;
+
+        // 生成HTML文件
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'HTML');//将$objPHPExcel对象转换成html格式的
+        $objWriter->setSheetIndex(0);  //设置活动表
+        //$objWriter->setImagesRoot('http://www.example.com');
+        $objWriter->save(str_replace('.php', '.htm', __FILE__));//保存文件
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
