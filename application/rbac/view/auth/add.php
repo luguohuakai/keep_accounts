dc49b3f97c60936777b0ed286f489c1d65ddb01a
DM <769245396@qq.com>
2017-06-05 00:38:21 +0800

    为角色分配权限

--- application/rbac/view/auth/add.php
+++ application/rbac/view/auth/add.php
@@ -1,4 +1,3 @@
-
 <div class="panel panel-default">
     <div class="panel-heading">
         <h3 class="panel-title">auth 添加</h3>
@@ -7,19 +6,19 @@
         <form class="form-horizontal" method="post" action="{:url('add_handle')}">
 
             {:token()}
-                                                                            <div class="form-group">
+            <div class="form-group">
                 <label class="control-label col-sm-2" for="name">权限名称</label>
                 <div class="col-sm-8">
                     <input type="text" class="form-control" id="name" name="name" style="">
                 </div>
             </div>
-                                        <div class="form-group">
+            <div class="form-group">
                 <label class="control-label col-sm-2" for="rule">路由规则</label>
                 <div class="col-sm-8">
                     <input type="text" class="form-control" id="rule" name="rule" style="">
                 </div>
             </div>
-                                                                                                                                                    <div class="form-group">
+            <div class="form-group">
                 <label class="control-label col-sm-2" for="group_id">权限组ID</label>
                 <div class="col-sm-8">
                     <select class="form-control" name="group_id" id="group_id">
@@ -29,7 +28,7 @@
                     </select>
                 </div>
             </div>
-                        <div class="form-group">
+            <div class="form-group">
                 <label class="control-label col-sm-2"></label>
                 <div class="col-sm-4">
                     <button type="submit" class="form-control btn btn-primary">确认</button>
